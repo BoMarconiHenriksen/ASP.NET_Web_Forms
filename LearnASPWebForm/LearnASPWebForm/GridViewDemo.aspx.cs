67afbb853e19644d7e8d2dd06975f6d494969e0d
BoMarconiHenriksen <bomarconi@gmail.com>
2019-08-22 15:40:48 +0200

    Example of GridView and BulletList. Also called item controls

---
+++ LearnASPWebForm/LearnASPWebForm/GridViewDemo.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LearnASPWebForm
+{
+    public partial class GridViewDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<string> names = new List<string>();
+            for (int i = 0; i < 99; i++)
+            {
+                names.Add("John Doe" + i.ToString());
+            }
+            // CustomerGridView.DataSource = names;
+            // Bind the data into the GridView table. This is called an Item control-
+            // CustomerGridView.DataBind();
+
+            // For the bullet list.
+            CustomersList.DataSource = names;
+            CustomersList.DataBind();
+        }
+    }
+}
