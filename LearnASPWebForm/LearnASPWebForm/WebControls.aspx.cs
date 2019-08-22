4b245fc5534502875e0f8d86fa863429dc8ba30e
BoMarconiHenriksen <bomarconi@gmail.com>
2019-08-22 15:25:45 +0200

    Webcontrol example

---
+++ LearnASPWebForm/LearnASPWebForm/WebControls.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LearnASPWebForm
+{
+    public partial class WebControls : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            // Get the input from the first name textbox.
+            var fname = FirstNameTextBox.Text;
+            var lname = LastNameTextBox.Text;
+            var state = StatesDropDown.SelectedValue;
+
+            OutputLabel.Text = fname + " " + lname + " " + state;
+        }
+    }
+}
