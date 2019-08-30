4aeb2d4bdbffe40696498acd198fd13967ab739d
BoMarconiHenriksen <bomarconi@gmail.com>
2019-08-30 10:16:21 +0200

    Added gridview and detail gridview

--- LearnASPWebForm/LearnASPWebForm/DataSourceControlsDemo1.aspx.designer.cs
+++ LearnASPWebForm/LearnASPWebForm/DataSourceControlsDemo1.aspx.designer.cs
@@ -29,5 +29,32 @@ namespace LearnASPWebForm {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// GridView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridView1;
+        
+        /// <summary>
+        /// CustomersSqlDataSource control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource CustomersSqlDataSource;
+        
+        /// <summary>
+        /// DetailsView1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DetailsView DetailsView1;
     }
 }
